90b02ad769b45ea06fdf3f05e97694aac152f9cd
kataja1 <krissgutmane@gmail.com>
2024-10-04 11:44:35 +0300

    init

---
+++ mjau.py
@@ -0,0 +1,20 @@
+a = 15
+b = 10
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a//b)
+print(a%b)
+
+a = int(input("Enter number a:"))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a//b)
+print(a%b)
