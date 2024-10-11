a3bbe8db26b4fd5c55cd7d1368e7fe2261589d1c
kataja1 <krissgutmane@gmail.com>
2024-10-11 12:13:15 +0300

    Create miu.1.py

---
+++ miu.1.py
@@ -0,0 +1,14 @@
+try:
+    a = int(input("Enter a:"))
+    b = int(input("Enter b:"))
+    c= int(input("Enter b:"))
+
+except ValueError:
+    print('a or b or c = int')
+
+
+D = b**2 - 4 * a * c
+x1= ( -b + D**0,5) / (2*a)
+x2 = ( -b - D**0,5 ) / (2*a)
+print(x1)
+print(x2)
